93c7ec5d5111d5f69b7ae1615de130f495b6ab2e
lmarek <marek.lucas@gmail.com>
2016-06-02 22:57:38 +0200

    Now /sat returns a proper JSON.

--- src/main/java/pl/edu/agh/api/SATResponse.java
+++ src/main/java/pl/edu/agh/api/SATResponse.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import pl.edu.agh.parser.transformation.CNFConverter;
 import pl.edu.agh.parser.validation.FormulaValidator;
 import pl.edu.agh.solver.facades.SolverFacade;
@@ -13,15 +15,24 @@ import java.util.Map;
 /**
  * Created by lmarek on 21.05.2016.
  */
+@JsonRootName("response")
 public class SATResponse implements SATResponseFrame {
     protected final static FormulaValidator validator = new FormulaValidator();
+    @JsonProperty
     protected final String formula;
+    @JsonProperty
     protected final String asCNF;
+    @JsonProperty
     protected final boolean satisfiable;
+    @JsonProperty
     protected final boolean parseFailed;
+    @JsonProperty
     protected final int errorCode;
+    @JsonProperty
     protected final char parserGotInstead;
+    @JsonProperty
     protected final long parserErrorPosition;
+    @JsonProperty
     protected final Map<String, Boolean> assignments;
 
     public boolean isSatisfiable() {
