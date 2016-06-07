43000a542502e1f091acf183c3824a0dc95d235d
tkumor3 <tkumor3@gmail.com>
2016-06-07 13:48:27 +0200

    UI

--- src/main/resources/static/js/jquery.js
+++ src/main/resources/static/js/jquery.js
@@ -15,9 +15,46 @@ function sat(form) {
         url: 'http://localhost:8080/sat',
         headers: { formula : form }
     }).done(function(res){
-        $(".list").append("<li> formula: "+form+ "  satisfiable: "+ res.satisfiable +" </li>");
 
+        if(res.parseFailed == true){
+           alert("error Code "+ res.errorCode +"  parser got instead " +
+               ""+res.parserGotInstead + " error position " + res.parserErrorPosition )
+
+        }else {
+
+            if(res.satisfable == true) {
+                $(".list").append("<li>satisfable: " + res.satisfable + " " + "formula: " + res.formula + " asCNF: " +
+                    "" + res.asCNF + " " + assignment(res.assignments) + " </li>");
+            }else{
+                $(".list").append("<li>satisfable: " + res.satisfable + " " + "formula: " + res.formula + " asCNF: " +
+                    "" + res.asCNF + "</li>");
+
+            }
+        };
     });
 
 
 };
+
+function assignment(assig){
+    var string="";
+    $.each(assig,function(index,value){
+        string = string + index+ ": " + value+" ";
+
+    });
+    return string;
+}
+
+/*
+parsefailed.
+    Jeśli ma wartość true, to wyświetlasz errorCode i parserGotInstead.
+(Errorcode będzie trzeba przerobić, każdemu numerkowi powinien towarzyszyć string, ale to później).
+Dodatkowo: input formula powinno zostać wyświetlone.
+    Jeśli natomiast parseFailed == false, ignorujesz parserGotInstead i errorcode.
+    W zamian wyświetlasz input, asCnf, satisfiable.
+    Jeśli satisfiable == true, wyświetlasz dodatkowo assignments.
+(Bo jak satisfiable == false, to assignments wynosi null).
+I to na razie byłoby tyle.
+
+
+    */
